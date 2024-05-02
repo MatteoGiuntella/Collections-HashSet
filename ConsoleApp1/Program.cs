b28ac4e99a6c8570e5edf5b20e41b1f832edc238
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-05-02 12:01:00 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+        HashSet<string> animali = new HashSet<string>()
+        {
+            "Cane", "Gatto", "Elefante", "Leone"
+        };
+
+        // Rimuovo l'animale "Elefante" dall'HashSet
+        animali.Remove("Elefante");
+
+        Console.WriteLine("HashSet aggiornato:");
+        foreach (var animale in animali)
+        {
+            Console.WriteLine(animale);
+        }
